c62ffad7de55c77bd2d5405750841c8d6c655b0d
Aaron Cance <aaron.cance@gmail.com>
2019-08-09 14:26:31 -0500

    facebook bulk read

--- bulk.php
+++ bulk.php
@@ -1,24 +1,32 @@
 <?php
-require __DIR__ . '/Facebook/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use FacebookAds\Object\Ad;
 use FacebookAds\Object\Lead;
 use FacebookAds\Api;
 use FacebookAds\Logger\CurlLogger;
 
-$access_token = 'EAAj5S3HZCjFEBAL01GC9yGd4CsPEnNYkFFQC6UteuMwALMzJabSZBKQVnWSzKd3vBuCNo5mfSxLq4LAyjd6mZCNqaT4ph01tG8S1GaTN8a1eC543NjG4S7rMdR9IVj1UiglFZBFkq80trKFfZCCMGAYfkZCI0qN6YW5ZBFui3i8ayiRAMgpsvQYMHXTAqsX0GkZD';
-$app_secret = 'ddf0c9cf79112ad00f6bd2995551fc27';
-$app_id = '2525902244121681';
-$id = '406744446638176';
+if(!defined('STDIN'))  define('STDIN',  fopen('php://stdin',  'r'));
+if(!defined('STDOUT')) define('STDOUT', fopen('php://stdout', 'w'));
+if(!defined('STDERR')) define('STDERR', fopen('php://stderr', 'w'));
 
-$api = Api::init($app_id, $app_secret, $access_token);
+$ACCESS_TOKEN = 'EAAj5S3HZCjFEBAL01GC9yGd4CsPEnNYkFFQC6UteuMwALMzJabSZBKQVnWSzKd3vBuCNo5mfSxLq4LAyjd6mZCNqaT4ph01tG8S1GaTN8a1eC543NjG4S7rMdR9IVj1UiglFZBFkq80trKFfZCCMGAYfkZCI0qN6YW5ZBFui3i8ayiRAMgpsvQYMHXTAqsX0GkZD';
+$APP_SECRET = 'ddf0c9cf79112ad00f6bd2995551fc27';
+$APP_ID = '2525902244121681';
+$FORM_ID = '406744446638176';
+
+$api = Api::init($APP_ID, $APP_SECRET, $ACCESS_TOKEN);
 $api->setLogger(new CurlLogger());
 
 $fields = array(
 );
 $params = array(
 );
-echo json_encode((new Ad($id))->getLeads(
+echo json_encode((new Ad($FORM_ID))->getLeads(
   $fields,
   $params
 )->getResponse()->getContent(), JSON_PRETTY_PRINT);
+
+//var_export(new Ad($FORM_ID));
+
+//var_dump(json_encode(new Ad($FORM_ID)));
